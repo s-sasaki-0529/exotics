58128026935ae59e7f592b200694bc9eab898205
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2017-10-14 16:36:59 +0000

    VueElementを廃止して、OnsenUIを使うように変更

--- app/javascript/index.vue
+++ app/javascript/index.vue
@@ -1,17 +1,21 @@
-<template>
+<template id="main-page">
+  <v-ons-page>
+    <v-ons-toolbar>
+      <div class="center">Title</div>
+    </v-ons-toolbar>
 
-<div id="index">
-  {{ data }}
-</div>
-
+    <p style="text-align: center">
+      <v-ons-button @click="$ons.notification.alert('Hello World!')">
+        Click me!
+      </v-ons-button>
+    </p>
+  </v-ons-page>
 </template>
 
 <script>
-  export default {
-    data: () => {
-      return {
-        data: 'Hello,Vue Component!!',
-      }
-    }
-  }
+  import Vue from 'vue'
+  var vm = new Vue({
+    el: '#main-page',
+    template: '#main-page'
+  });
 </script>
--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,10 +1,10 @@
 import Vue        from 'vue';
-import ElementUI  from 'element-ui'
-import locale     from 'element-ui/lib/locale/lang/ja'
-import 'element-ui/lib/theme-default/index.css'
 import VueRouter  from 'vue-router';
+import VueOnsen from 'vue-onsenui';
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
 
-Vue.use(ElementUI, {locale});
+Vue.use(VueOnsen);
 Vue.use(VueRouter);
 
 // ページごとのコンポーネントを定義
