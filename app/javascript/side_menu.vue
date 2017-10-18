65431b6cb619159f5dc569de2ce395363e007d2c
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2017-10-18 11:10:43 +0000

    サイドメニューを動的に生成するように

--- app/javascript/side_menu.vue
+++ app/javascript/side_menu.vue
@@ -2,8 +2,8 @@
   <v-ons-splitter-side swipeable width="150px" collapse="" :open="isOpen" @preclose="$emit('slideSideMenu', false)">
     <v-ons-page>
       <v-ons-list>
-        <v-ons-list-item tappable modifier="chevron" @click="$emit('slideSideMenu')">
-          トップ
+        <v-ons-list-item v-for="menu in menus" tappable modifier="chevron" @click="$emit('slideSideMenu')">
+          {{ menu.label }}
         </v-ons-list-item>
       </v-ons-list>
     </v-ons-page>
@@ -16,6 +16,12 @@
     props: ['isOpen'],
     data() {
       return {
+        menus: [
+          {key: 'top',    isShow: true, label: 'トップ'},
+          {key: 'mypage', isShow: true, label: 'マイページ'},
+          {key: 'owners', isShow: true, label: '飼い主一覧'},
+          {key: 'logout', isShow: true, label: 'ログアウト'},
+        ]
       }
     }
   }
