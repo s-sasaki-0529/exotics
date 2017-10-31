d33a4bfd560aadafe7128a3d44b4fb6496cd25b1
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2017-10-31 15:07:42 +0000

    頑張ってタブを実装

--- app/javascript/index.vue
+++ app/javascript/index.vue
@@ -14,10 +14,11 @@
         <exotics-header @slideSideMenu="slideSideMenu"/>
 
         <!-- タブメニュー -->
-        <v-ons-tabbar swipeable id="appTabbar" position="auto">
-          <v-ons-tab label="オーナー" icon="fa-user"></v-ons-tab>
-          <v-ons-tab label="ペット" icon="fa-paw"></v-ons-tab>
-          <v-ons-tab label="ブログ" icon="fa-book" ></v-ons-tab>
+        <v-ons-tabbar swipeable position="auto" :visible="tabVisible" :index.sync="tabIndex">
+          <v-ons-tab v-for="tab in tabs"
+            :icon="tab.icon"
+            :label="tab.label"
+          />
         </v-ons-tabbar>
 
       </v-ons-page>
@@ -33,6 +34,25 @@
     data() {
       return {
         isSideOpening: false,
+        tabIndex: 1,
+        tabVisible: true,
+        tabs: [
+          {
+            icon: 'fa-user',
+            label: 'オーナー',
+            key: 'owner',
+          },
+          {
+            icon: 'fa-paw',
+            label: 'ペット',
+            key: 'owner',
+          },
+          {
+            icon: 'fa-book',
+            label: 'ブログ',
+            key: 'blog',
+          }
+        ]
       }
     },
     methods: {
