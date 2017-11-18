5decc36c909b63797962b1f4d36b5ce1de559abc
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2017-11-18 15:57:18 +0000

    carousel_dotコンポーネントを追加

---
+++ app/javascript/parts/carousel_dots.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="carousel-dots">
+    <span v-for="i in length" :key="i" style="cursor: pointer">
+      {{ i - 1 ===  index ? '\u25CF' : '\u25CB' }}
+    </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      length: {
+        type: Number,
+        required: true,
+      },
+      index: {
+        type: Number,
+        required: true,
+      },
+    }
+  }
+</script>
