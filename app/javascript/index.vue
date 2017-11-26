d885ad9c24b505480434b0282778ba650462c6d6
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2017-11-26 03:42:42 +0000

    タブの利用を取りやめ。タブごとにナビゲーションを導入するとよくわからない挙動になるので

--- app/javascript/index.vue
+++ app/javascript/index.vue
@@ -1,41 +1,19 @@
 <template>
   <v-ons-page>
-    <v-ons-tabbar position="auto" :visible="true" :tabs="tabs" :index.sync="tabIndex">
-    </v-ons-tabbar>
+    <OwnerNavigator />
   </v-ons-page>
 </template>
 
 <script>
-  import owner_navigator from './owner/owner_navigator'
-  import pets  from './owner/pets'
-  import home  from './home/home'
-  import setting from './setting/setting'
+  import OwnerNavigator from './owner/owner_navigator'
   export default {
     name: 'index',
     data() {
       return {
-        tabIndex: 0,
-        tabs: [
-          {
-            icon: 'fa-home',
-            label: 'ホーム',
-            page: home,
-          },
-          {
-            icon: 'fa-user',
-            label: 'オーナー',
-            page: owner_navigator,
-          },
-          {
-            icon: 'fa-cog',
-            label: '設定',
-            page: setting,
-          }
-        ]
       }
     },
     methods: {
     },
-    components: { owner_navigator, pets, home, setting },
+    components: { OwnerNavigator },
   }
 </script>
