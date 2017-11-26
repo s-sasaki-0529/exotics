1eb06e56f4043714f74736b0d3d64a61a40b30a8
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2017-11-26 11:54:35 +0000

    ペット一覧のカルーセルの表示が３で固定になってたので修正

--- app/javascript/owner/pets.vue
+++ app/javascript/owner/pets.vue
@@ -1,13 +1,13 @@
 <template>
   <v-ons-page class="pets ex-center">
     <toolbar title="ペット一覧" backButton="オーナー"/>
-    <carousel-dots :length="3" :index.sync="current_index"/>
+    <carousel-dots :length="pets.length" :index.sync="current_index"/>
     <v-ons-carousel swipeable auto-scroll :auto-scroll-ratio="0.1" :index.sync="current_index">
       <v-ons-carousel-item v-for="(pet, idx) in pets">
         <pet :key="idx" v-bind="pet" />
       </v-ons-carousel-item>
     </v-ons-carousel>
-    <carousel-dots :length="3" :index.sync="current_index"/>
+    <carousel-dots :length="pets.length" :index.sync="current_index"/>
   </v-ons-page>
 </template>
 
