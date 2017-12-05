4fd6d0a8c4ffebd99b642220396a2c02db0b8949
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2017-12-05 08:50:34 +0000

    seederにサンプルユーザを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# sample Users
+User.create(
+  provider: 'twitter',
+  uid: '341992852',
+  nickname: 'Sa2Knight',
+  image_url: 'http://pbs.twimg.com/profile_images/709702386550308864/0bKYD-gI_normal.jpg'
+)
