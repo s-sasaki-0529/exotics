9e84295e0e526856148f07aa68c5e3c21f3bbf22
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2017-12-05 14:14:13 +0000

    seederにデグー３匹追加

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,5 @@
 class Pet < ApplicationRecord
   belongs_to :user
   belongs_to :kind
+  enum gender: { male: 0, female: 0 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # sample Users
-User.create(
+user = User.create(
   provider: 'twitter',
   uid: '341992852',
   nickname: 'Sa2Knight',
@@ -8,3 +8,22 @@ User.create(
 
 # sample kind
 degu = Kind.create(name: 'デグー')
+
+# sample pets
+Pet.create([
+  {
+    user: user, kind: degu, name: 'パズー', gender: 'male', birthday: '2015/12/01',
+    description: '我が家最初のデグー',
+    image_url: 'https://pbs.twimg.com/media/Ct0F1uZUsAEvqAm.jpg',
+  },
+  {
+    user: user, kind: degu, name: 'メイ', gender: 'female', birthday: '2016/02/01',
+    description: '二度の出産経験者',
+    image_url: 'https://pbs.twimg.com/media/CuzjROnUMAAVrBn.jpg',
+  },
+  {
+    user: user, kind: degu, name: 'ジジ', gender: 'male', birthday: '2016/09/01',
+    description: 'グルメで好き嫌い多め',
+    image_url: 'https://pbs.twimg.com/media/DFFzOumU0AAi-Ew.jpg',
+  },
+])
