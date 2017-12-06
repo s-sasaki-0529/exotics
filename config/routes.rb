194458a276683823ce307e2779c74cbf46a02e42
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2017-12-06 14:24:38 +0000

    ユーザ一覧を取得するAPIのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get '/' => 'home#index'
   get '/auth/:provider/callback' => 'sessions#create'
+
+  # API
+  namespace :api, defaults: { format: :json } do
+    resources :user, only: [:index]
+  end
 end
