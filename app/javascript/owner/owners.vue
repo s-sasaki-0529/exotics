ac82b76247c23dd86809d7572681beaf05fd54f3
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2017-12-10 10:04:44 +0000

    オーナー一覧をAPIで取得して描画するように

--- app/javascript/owner/owners.vue
+++ app/javascript/owner/owners.vue
@@ -4,11 +4,11 @@
     <v-ons-lit>
       <v-ons-list-item v-for="owner in owners" tappable :key="owner.id" @click="select(owner)">
         <div class="left">
-          <img class="list-item__thumbnail" :src="owner.thumbnail">
+          <img class="list-item__thumbnail" :src="owner.image_url">
         </div>
         <div class="center">
-          <span class="list-item__title">{{ owner.name }}</span>
-          <span class="list-item__subtitle">{{ owner.pets }}</span>
+          <span class="list-item__title">{{ owner.nickname }}</span>
+          <span class="list-item__subtitle">デグー</span>
         </div>
         <hr>
       </v-ons-list-item>
@@ -17,13 +17,19 @@
 </template>
 
 <script>
+  import http  from '../common/http'
   import owner from './owner'
   import dummy_owners from '../dummy/owners'
   export default {
     data() {
       return {
-        owners: dummy_owners,
+        owners: [],
       }
+    },
+    mounted() {
+      http.getUsers((data) => {
+        this.owners = data
+      })
     },
     methods: {
       select(selectedOwner) {
@@ -31,6 +37,6 @@
         this.$emit('push-page', owner)
       }
     },
-    components: {owner}
+    components: {owner},
   }
 </script>
