3add7bee783252c59cdc2cf6ea11620ef2b90263
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2017-12-10 10:19:03 +0000

    UsersテーブルにIntroduction列を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205090326) do
+ActiveRecord::Schema.define(version: 20171210101749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20171205090326) do
     t.string "uid"
     t.string "nickname"
     t.string "image_url"
+    t.text "introduction"
   end
 
   add_foreign_key "pets", "kinds"
