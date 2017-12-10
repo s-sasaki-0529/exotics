e9d7c8b8a9866003e21d173dcfd585caf39b0646
Shingo Sasaki <shingo.sasaki.0529@gmail.com>
2017-12-10 10:30:12 +0000

    UserSerializerをUserOverviewSerializerにリネーム。詳細取得用のシリアライザは別途作るため

--- app/serializers/user_overview_serializer.rb
+++ app/serializers/user_overview_serializer.rb
@@ -1,3 +1,3 @@
-class UserSerializer < ActiveModel::Serializer
+class UserOverviewSerializer < ActiveModel::Serializer
   attributes :id, :nickname, :image_url, :introduction
 end
--- app/stylesheet/api/user_controller.rb
+++ app/stylesheet/api/user_controller.rb
@@ -4,7 +4,7 @@ class Api::UserController < Api::ApplicationController
   # ユーザ一覧を取得
   #
   def index
-    render json: User.all, each_serializer: UserSerializer
+    render json: User.all, each_serializer: UserOverviewSerializer
   end
 
 end
